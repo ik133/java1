cb0875d90065ed2bebb95e6a452a660bb4a1af67
mik543765 <mik543765@gmail.com>
2023-08-09 07:13:50 -0700

    java1

---
+++ app.js
@@ -0,0 +1,83 @@
+                    // Chapter # 1
+
+// var Fullname ='Error! Please Enter a Valid Password';
+// alert (Fullname)
+
+// var Fullname ="welcome to js land..\nHappy Coding!"
+// alert(Fullnamne)
+        
+         
+            //  Chapter # 2
+// var username;
+// var myname = 'your Full Name ';
+
+// var message ="Hello World";
+// alert (message)
+
+// var studentName ="john Doe";
+// var age =20;
+// var SchoolName ="Sent Lawrence";
+// var grade ="10th Grade";
+// alert ("student Name: " + studentName + "\n age: " + age + "\nSchoolName :" + SchoolName + "\ngrade :" + grade);
+
+// var alertmessaege = "Welcome to my website ! \n i hope you have a greaat experience.";
+// alert(alertmessaege);
+
+// var email ="your_@email.com";
+// alert("My email address is:" + email);
+// var book = "A smarter way to learn JavaScript";
+// alert("Book Title: " + book);
+// var decorativeString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// Display in an alert box
+// alert(decorativeString);
+
+// Display in the browser
+// document.write(decorativeString + "<br>-- END --");
+
+
+                //    CHAPTER # 3
+
+            //     LegalVariable= Names;
+
+            //     myVariable
+            //     name123
+            //     _underscore
+            //     $dollarSign
+            //     myVar_2
+            
+            //     illegalVariable= Names;
+                
+            //    firstname= ('starts with a number')
+            //     my-variable ("contains a hyphen")
+            //      secondname ("contains a space")
+            //     function= ("reserved keyword")
+            //     myvariable ("contains a space")
+            //     Display in Browser;
+
+
+                    // CHAPTER # 5
+// var num1 = parseFloat(prompt("Enter the first number:"));
+// var num2 = parseFloat(prompt("Enter the second number:"));
+// var result = num1 + num2;
+// document.write("Addition result: " + result);
+
+// var num1 = parseFloat(prompt("Enter the first number:"));
+// var num2 = parseFloat(prompt("Enter the second number:"));
+// var result = num1 - num2;
+// document.write("Subtraction result: " + result);
+
+// var num1 = parseFloat(prompt("Enter the first number:"));
+// var num2 = parseFloat(prompt("Enter the second number:"));
+// var result = num1 * num2;
+// document.write("Multiplication result: " + result);
+
+// var num1 = parseFloat(prompt("Enter the first number:"));
+// var num2 = parseFloat(prompt("Enter the second number:"));
+// var result = num1 / num2;
+// document.write("Division result: " + result);
+
+
+
+//                         THE END
+
